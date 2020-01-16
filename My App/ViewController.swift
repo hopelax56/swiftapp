f067387961a41078232fd068b6b28e8eb5c8e8f7
Ross Mika <hopelax56@comcast.net>
2020-01-16 12:33:05 -0600

    Initial Commit

---
+++ My App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My App
+//
+//  Created by Ross Mika on 1/16/20.
+//  Copyright © 2020 Ross Mika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
