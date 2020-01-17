ed0137af2f6971002ed06f78f605538379760b4b
Ross Mika <hopelax56@comcast.net>
2020-01-17 14:35:23 -0600

    Boolean section

--- My App/ViewController.swift
+++ My App/ViewController.swift
@@ -18,16 +18,22 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        Gotcha.text = "Answer is...\(Double(Text.text!)! + Double(text2.text!)!)"
+        var addition = false
         
-    }
+        if addition {
+            
+            Gotcha.text = "Answer is...\(Double(Text.text!)! + Double(text2.text!)!)"
+        } else {Gotcha.text = "Answer is...\(Double(Text.text!)! - Double(text2.text!)!)"
+        }}
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+       
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+    }}
 
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
