84c3539dda283a93de25b7011310fa32bdb700ac
Ross Mika <hopelax56@comcast.net>
2020-01-17 12:15:20 -0600

    My first Commit

--- My App/ViewController.swift
+++ My App/ViewController.swift
@@ -3,13 +3,28 @@
 //  My App
 //
 //  Created by Ross Mika on 1/16/20.
-//  Copyright © 2020 Ross Mika. All rights reserved.
+//  Copyright © 2020 Ross Mika. All rights reserved...
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Gotcha: UILabel!
+    
+    var tapCount=0
+   
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10{
+            Gotcha.text = "Fuck off!"
+        }
+    }
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +32,3 @@ class ViewController: UIViewController {
 
 
 }
-
